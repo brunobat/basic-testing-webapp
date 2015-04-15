708a09ee81ce6f50da99c0f2c43dba993b0d02d9
Bruno Baptista <brunobat@gmail.com>
2015-04-16 00:18:25 +0100

    Rest working

--- src/test/java/com/brunobat/service/resource/AccountResourceIntegrationTest.java
+++ src/test/java/com/brunobat/service/resource/AccountResourceIntegrationTest.java
@@ -2,20 +2,12 @@ package com.brunobat.service.resource;
 
 import com.brunobat.model.FinancialTransaction;
 import com.brunobat.model.Owner;
-import com.brunobat.service.AccountService;
 import com.brunobat.service.repository.OwnerJPARepository;
-import com.brunobat.service.resource.AccountResource;
 import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.extension.rest.client.ArquillianResteasyResource;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.arquillian.test.api.ArquillianResource;
-import org.jboss.shrinkwrap.api.ArchivePaths;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.jboss.shrinkwrap.descriptor.api.Descriptors;
-import org.jboss.shrinkwrap.descriptor.api.persistence10.PersistenceDescriptor;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -24,7 +16,6 @@ import org.junit.runner.RunWith;
 import javax.inject.Inject;
 import javax.ws.rs.core.Response;
 import java.io.File;
-import java.net.URL;
 import java.util.ArrayList;
 
 /**
@@ -41,7 +32,7 @@ public class AccountResourceIntegrationTest {
 
     @Deployment()
     public static WebArchive createDeployment() {
-                return ShrinkWrap.createFromZipFile(WebArchive.class, new File(
+        return ShrinkWrap.createFromZipFile(WebArchive.class, new File(
                 "target/basicWebapp.war"));
     }
 
